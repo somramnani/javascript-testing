357a34cdb241483a9ee5ab68fe0c1a1ca4123e3f
somramnani <som.ramnani@gmail.com>
2024-10-23 18:57:43 -0400

    Add test for max function

---
+++ tests/intro.test.js
@@ -0,0 +1,14 @@
+import { describe, test, it, expect } from "vitest";
+import { max } from "../src/intro";
+
+describe("max", () => {
+  it("should return the first argument if it is greater", () => {
+    expect(max(2, 1)).toBe(2);
+  });
+  it("should return the second argument if it is greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+  it("should return first arguments are equal", () => {
+    expect(max(1, 1)).toBe(1);
+  });
+});
